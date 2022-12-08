cebbda8be2b793e40418e8859cbbac0d43e9de66
kelvin-pun <kelvin.pun@gmail.com>
2022-12-08 14:52:23 +0800

    Complete readme Add code validation

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # EPSCO Barcode Generator
-Calculate the check digit and generate barcode for PPS
+
+##Calculate the check digit and generate barcode for PPS
 
 >Barcode symbology of Code 128 Set B is recommended. Code 128 is very high density alphanumeric barcode,though EPSCO barcode template uses numeric only. The X-dimension (the dimension of the narrowest element of barcode symbol) of 7.5 mils is recommended. 7.5 mils equal 0.0075 inch. Additional information about Code 128 is available e.g. at www.idautomation.com or www.barcodingfonts.com
 
@@ -19,3 +20,8 @@ The validation algorithm will be as follows:
 - The check digit will be [Module - MOD (Total sum of Digits, Module)]
 - If MOD (Total sum of Digits, Module) = 0 then check digit is equal to 0
 
+### Docker build and run
+```bash
+docker build -t epsco_barcode .
+docker run -d --name my_epsco -p 80:80 epsco_barcode
+```
--- app/main.py
+++ app/main.py
@@ -1,9 +1,10 @@
 import asyncio
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from fastapi.responses import StreamingResponse
 from io import BytesIO
 import barcode
 from barcode.writer import ImageWriter
+import re
 
 def sum_of_digits(digit :int) -> int:
     return 0 if digit == 0 else int(digit % 10) + sum_of_digits(int(digit / 10))
@@ -15,13 +16,19 @@ app = FastAPI()
 
 @app.get("/{code}")
 async def gen_barcode(code: str):
+    pattern = re.compile("^[0-9]{15}$")
+    if not pattern.match(code):
+        raise HTTPException(status_code=418, detail="The barcode code format incorrect")
     headers = {
         'Content-Dispostion': 'inline; filename="pps.png"'
     }
     rv = BytesIO()
+    options = {
+        'font_size': 6,
+        'text_distance': 3
+    }
     code128 = barcode.get_barcode_class('code128')
-    print(code+str(check_digit(code)))
-    #code128(code + str(check_digit(code)), writer=ImageWriter()).write(rv)
+    code128(code + str(check_digit(code)), writer=ImageWriter()).write(rv, options = options)
     rv.seek(0)
     return StreamingResponse(rv, headers=headers,media_type="image/png")
 
